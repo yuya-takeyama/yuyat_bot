48db63d0db78afd56f122ad36de2ed93ec95907c
Yuya Takeyama <sign.of.the.wolf.pentagram@gmail.com>
2012-04-30 17:00:47 +0900

    Setup RSpec environment.

--- spec/unit/lib/yuyat_bot_spec.rb
+++ spec/unit/lib/yuyat_bot_spec.rb
@@ -1,5 +1,3 @@
-require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
-
 describe "YuyatBot" do
   it "fails" do
     fail "hey buddy, you should probably rename this file and start specing for real"
